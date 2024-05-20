8ee506cd41d1996deb0477ecb5792d55def898d5
Developers Today <mshari7185@gmail.com>
2024-05-20 22:16:47 +0530

    made mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express();
@@ -9,6 +9,14 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 
+const mongoUri = process.env.MONGODB_URI;
+if (!mongoUri) {
+  throw new Error('MONGODB_URI is not defined in the environment variables');
+}
+
+// Connect to MongoDB
+mongoose.connect(mongoUri).then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 app.get('/', (req, res) => res.send('API Running'));
