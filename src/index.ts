074f03ebc7d381adb8552955beb28c19ff25f9b7
Developers Today <mshari7185@gmail.com>
2024-05-21 22:51:05 +0530

    connected to mongodb via prisma

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import prisma from '../prisma/prisma';  // Import the prisma client
+
 dotenv.config();
 
 const app = express();
@@ -9,14 +10,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 
-const mongoUri = process.env.MONGODB_URI;
-if (!mongoUri) {
-  throw new Error('MONGODB_URI is not defined in the environment variables');
-}
-
-// Connect to MongoDB
-mongoose.connect(mongoUri).then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+// Connect to MongoDB (via Prisma)
+prisma.$connect()
+  .then(() => console.log('Connected to MongoDB via Prisma'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Routes
 app.get('/', (req, res) => res.send('API Running'));
