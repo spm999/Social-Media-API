109e25f3c37a2a7b6b2634abbc4c07f6c076aa33
Developers Today <mshari7185@gmail.com>
2024-05-30 21:49:24 +0530

    added post route to update image

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,16 @@
 // src/routes/userRoutes.ts
 import { Router } from 'express';
-import { registerUser, loginUser } from '../controllers/userController';
+import { registerUser, loginUser, updateUserBio, updateProfileImage } from '../controllers/userController';
 import { authMiddleware } from '../middleware/auth';
+import multer from 'multer';
 
 const router = Router();
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
 
 router.post('/register', registerUser);
 router.post('/login',authMiddleware, loginUser);
-// router.get('/profile', authMiddleware, getUserProfile);
+router.put('/update-bio',authMiddleware, updateUserBio);
+router.post('/update-pimg', upload.single('image'), authMiddleware, updateProfileImage)
 
 export default router;
