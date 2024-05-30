5f7c9250350d790801694fc598c23cbc0d2f627b
Developers Today <mshari7185@gmail.com>
2024-05-30 21:50:23 +0530

    added middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import prisma from './prisma';  // Import the prisma client
-
+import bodyParser from 'body-parser';
 import userRoutes from './routes/user';
-
 
 dotenv.config();
 
@@ -11,12 +10,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true,}));
 
-// Connect to MongoDB (via Prisma)
+// Connect to Prisma (or MongoDB in your case)
 prisma.$connect()
   .then(() => console.log('Connected to MongoDB via Prisma'))
   .catch((err) => console.error('MongoDB connection error:', err));
+
+
 
 // Routes
 app.use('/api/users', userRoutes);  // Use user routes
