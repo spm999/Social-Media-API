8d933a218d8336eb191ec0bdabacaec735a61549
Developers Today <mshari7185@gmail.com>
2024-05-30 23:15:40 +0530

    post related routes call

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import prisma from './prisma';  // Import the prisma client
 import bodyParser from 'body-parser';
 import userRoutes from './routes/user';
+import postRoutes from './routes/post'
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ prisma.$connect()
 
 // Routes
 app.use('/api/users', userRoutes);  // Use user routes
+app.use('/api/users', postRoutes);  // Use user routes
 app.get('/', (req, res) => res.send('API Running'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
