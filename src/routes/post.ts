a376bc8643b6f8057a48b7bf6d5cc8ca75488d83
Developers Today <mshari7185@gmail.com>
2024-06-04 23:26:50 +0530

    put route to update post, with single image

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -2,13 +2,16 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middleware/auth';
 import {createPost, getAllPublicPosts, updatePostById, deletePostById, getAllPosts} from "../controllers/postController"
+import multer from 'multer';
 
 const router = Router();
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
 
-router.post('/create-post', authMiddleware, createPost);
-router.post('/all-posts', getAllPublicPosts);
-router.post('/auth-all-posts',authMiddleware, getAllPosts);
-router.put('/update-post',authMiddleware, updatePostById);
+router.post('/create-post', upload.single('image'), authMiddleware, createPost);
+router.get('/all-posts', getAllPublicPosts);
+router.get('/auth-all-posts',authMiddleware, getAllPosts);
+router.put('/update-post/:id', upload.single('image'), authMiddleware, updatePostById);
 router.post('/delete-post', authMiddleware, deletePostById)
 
 export default router;
