1cc82611c937ecfe19ba18d168eef3b86eac3964
Developers Today <mshari7185@gmail.com>
2024-06-05 23:31:30 +0530

    reject request and get all friends routes

--- src/routes/friendship.ts
+++ src/routes/friendship.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { sendFriendRequest, acceptFriendRequest } from '../controllers/friendshipController';
+import { sendFriendRequest, acceptFriendRequest, rejectFriendRequest, getAllFriends } from '../controllers/friendshipController';
 import { authMiddleware } from '../middleware/auth';
 
 const router = Router();
 
 router.post('/friend-request', authMiddleware, sendFriendRequest);
 router.post('/friend-request/accept', authMiddleware, acceptFriendRequest);
+router.post('/friend-request/reject', authMiddleware, rejectFriendRequest);
+router.get('/friends', authMiddleware, getAllFriends);
 
 
 export default router;
