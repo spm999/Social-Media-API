2fcf20ee519d20d09eac7a627791acd424b1eb3e
Developers Today <mshari7185@gmail.com>
2024-06-05 23:17:41 +0530

    post route to accept request

--- src/routes/friendship.ts
+++ src/routes/friendship.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { sendFriendRequest } from '../controllers/friendshipController';
+import { sendFriendRequest, acceptFriendRequest } from '../controllers/friendshipController';
 import { authMiddleware } from '../middleware/auth';
 
 const router = Router();
 
 router.post('/friend-request', authMiddleware, sendFriendRequest);
+router.post('/friend-request/accept', authMiddleware, acceptFriendRequest);
 
 
 export default router;
