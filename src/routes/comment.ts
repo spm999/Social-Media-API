beaba4de77ddc6615ee86b43f1c1b48052508415
Developers Today <mshari7185@gmail.com>
2024-06-09 00:01:33 +0530

    put route for updateing comment

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middleware/auth';
-import { createComment, getCommentById, getCommentsByPostId } from '../controllers/commentController';
+import { createComment, getCommentById, getCommentsByPostId , updateComment} from '../controllers/commentController';
 
 const router = Router();
 
 router.post('/create-comment/:postId', authMiddleware, createComment);
 router.get('/:commentId', authMiddleware, getCommentById);
 router.get('/post/:postId', authMiddleware, getCommentsByPostId);
+router.put('/post/:commentId', authMiddleware, updateComment);
 
 
 export default router;
