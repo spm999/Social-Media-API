df224ed63f3987f169344d86543eb93fb1f9c365
Developers Today <mshari7185@gmail.com>
2024-06-08 23:04:50 +0530

    get comment controller

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -21,3 +21,28 @@ export const createComment = async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Failed to create comment' });
   }
 };
+
+
+export const getCommentById = async (req: Request, res: Response) => {
+  const commentId = req.params.commentId;
+  // console.log(commentId)
+  try {
+    const comment = await prisma.comment.findUnique({
+      where: { id: commentId },
+    });
+
+    if (!comment) {
+      return res.status(404).json({ error: 'Comment not found' });
+    }
+
+    res.status(200).json(comment);
+  } catch (error) {
+    console.error('Error fetching comment by ID:', error);
+    res.status(500).json({ error: 'Failed to fetch comment' });
+  }
+};
+
+
+
+
+
