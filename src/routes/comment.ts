2b6245557aff1095ea326a9efd65359a69fe53f3
Developers Today <mshari7185@gmail.com>
2024-06-09 00:06:20 +0530

    delete route

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middleware/auth';
-import { createComment, getCommentById, getCommentsByPostId , updateComment} from '../controllers/commentController';
+import { createComment, getCommentById, getCommentsByPostId , updateComment, deleteComment} from '../controllers/commentController';
 
 const router = Router();
 
@@ -8,6 +8,6 @@ router.post('/create-comment/:postId', authMiddleware, createComment);
 router.get('/:commentId', authMiddleware, getCommentById);
 router.get('/post/:postId', authMiddleware, getCommentsByPostId);
 router.put('/post/:commentId', authMiddleware, updateComment);
-
+router.delete('/post/:commentId', authMiddleware, deleteComment);
 
 export default router;
