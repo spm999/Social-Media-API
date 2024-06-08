6cd1eca261c3df780fec210928d2f72b03c8edde
Developers Today <mshari7185@gmail.com>
2024-06-08 23:50:46 +0530

    get route to fetch comment by post id

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middleware/auth';
-import { createComment, getCommentById } from '../controllers/commentController';
+import { createComment, getCommentById, getCommentsByPostId } from '../controllers/commentController';
 
 const router = Router();
 
 router.post('/create-comment/:postId', authMiddleware, createComment);
 router.get('/:commentId', authMiddleware, getCommentById);
+router.get('/post/:postId', authMiddleware, getCommentsByPostId);
 
 
 export default router;
