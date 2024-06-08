6c6af39a1ebc19216d8e77fb9fc7a3d6ee82b025
Developers Today <mshari7185@gmail.com>
2024-06-08 23:05:49 +0530

    get route for getting comment

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middleware/auth';
-import { createComment } from '../controllers/commentController';
+import { createComment, getCommentById } from '../controllers/commentController';
 
 const router = Router();
 
 router.post('/create-comment/:postId', authMiddleware, createComment);
+router.get('/:commentId', authMiddleware, getCommentById);
 
 
 export default router;
