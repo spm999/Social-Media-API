8fd45e95ad9b6f3ba5c9c40599cd5d22fa133420
Developers Today <mshari7185@gmail.com>
2024-06-10 22:36:23 +0530

    post route for remove like and get roy=ute to fetch likes by post id

--- src/routes/like.ts
+++ src/routes/like.ts
@@ -1,11 +1,13 @@
 // src/routes/likeRoutes.ts
 
 import express from 'express';
-import { addLike} from '../controllers/likeController';
+import { addLike, removeLike, getLikesByPostId} from '../controllers/likeController';
 import { authMiddleware } from '../middleware/auth';
 
 const router = express.Router();
 
 router.post('/addLike', authMiddleware, addLike);          // Add a like
+router.post('/removeLike', authMiddleware, removeLike);          // Add a like
+router.get('/like/post/:postId', authMiddleware, getLikesByPostId);  // Get likes by post
 
 export default router;
