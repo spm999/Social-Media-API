05e19a9f8adfe75f4015c40323f2016cb0d8ca61
Developers Today <mshari7185@gmail.com>
2024-06-15 22:04:18 +0530

    get route for getting messages

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -1,11 +1,13 @@
 // src/routes/messageRoutes.ts
 import express from 'express';
-import { sendMessage } from '../controllers/messageController';
+import { sendMessage, getMessages } from '../controllers/messageController';
 import { authMiddleware } from '../middleware/auth'; // Assume this middleware sets req.userId
 
 const router = express.Router();
 
 // Send a message
 router.post('/messages', authMiddleware, sendMessage);
+// Get messages between two users
+router.get('/messages/:otherUserId', authMiddleware, getMessages);
 
 export default router;
