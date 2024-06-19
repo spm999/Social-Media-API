4087c1a7658c4c51e9c48d4d4fabbb1bd9e02efa
Developers Today <mshari7185@gmail.com>
2024-06-19 11:46:18 +0530

    delete route for deleting group

--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createGroup, getGroupById, updateGroup} from '../controllers/groupController';
+import { createGroup, getGroupById, updateGroup,deleteGroup} from '../controllers/groupController';
 import { authMiddleware } from '../middleware/auth';
 
 const router = Router();
@@ -7,10 +7,7 @@ const router = Router();
 router.post('/groups', authMiddleware, createGroup); // Create a new group
 router.get('/groups/:groupId', authMiddleware, getGroupById); // Get a group by ID
 router.put('/groups/:groupId', authMiddleware, updateGroup); // Update a group
-
-
-
-
+router.delete('/groups/:groupId', authMiddleware, deleteGroup); // Delete a group
 
 
 
