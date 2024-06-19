ad1a053d69b05f8131914b7d89bbe20f4a1141bf
Developers Today <mshari7185@gmail.com>
2024-06-19 11:28:08 +0530

    get route for updating group detail

--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createGroup, getGroupById} from '../controllers/groupController';
+import { createGroup, getGroupById, updateGroup} from '../controllers/groupController';
 import { authMiddleware } from '../middleware/auth';
 
 const router = Router();
 
 router.post('/groups', authMiddleware, createGroup); // Create a new group
 router.get('/groups/:groupId', authMiddleware, getGroupById); // Get a group by ID
+router.put('/groups/:groupId', authMiddleware, updateGroup); // Update a group
 
 
 
